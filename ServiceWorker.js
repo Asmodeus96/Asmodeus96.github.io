02143dbba513c225ca34003044399c246e8a574a
Asmodeus96 <emerick.noel@gmail.com>
2019-11-20 11:40:54 +0100

    sv

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -19,10 +19,6 @@ var appShellFiles = [
 
 ];
 
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('/ServiceWorker.js');
-};
-
 self.addEventListener('install', (e) => {
   console.log('[Service Worker] Install');
   e.waitUntil(
