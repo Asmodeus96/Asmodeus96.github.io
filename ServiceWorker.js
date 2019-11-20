f1428d806c6e54f199aeda540714df8f0c3763e4
Asmodeus96 <emerick.noel@gmail.com>
2019-11-20 11:31:27 +0100

    sv

---
+++ ServiceWorker.js
@@ -0,0 +1,49 @@
+var cacheName = 'cachePWA';
+var appShellFiles = [
+  	'/icon/icon_192.png',
+	'/icon/icon_502.png',
+	'/images/barcode_scanner.png',
+	'/images/icon-cart.png',
+	'/images/icon-setup.png',
+	'/images/icon-transmite.png',
+	'/images/logo.png',
+	'/js/app.js',
+	'/js/DecoderWorker.js',
+	'/js/exif.js',
+	'/js/jobs',
+	'/favicon.ico',
+	'/index.html',
+	'/manifest.json',
+	'/produits.csv',
+	'/style.css'
+
+];
+
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/ServiceWorker.js');
+};
+
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching all: app shell and content');
+      return cache.addAll(contentToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => {
+          console.log('[Service Worker] Fetching resource: '+e.request.url);
+      return r || fetch(e.request).then((response) => {
+                return caches.open(cacheName).then((cache) => {
+          console.log('[Service Worker] Caching new resource: '+e.request.url);
+          cache.put(e.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
